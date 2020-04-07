05a502b81defba49205265b21bdcbd2006038528
Huỳnh Thế Anh <55992534+htheanh2000@users.noreply.github.com>
2020-04-07 16:16:20 +0700

    f1
    Final1

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+    $("#onTop").click(function(){
+        $('html,body').animate({scrollTop: 0},800);
+    });
+});
